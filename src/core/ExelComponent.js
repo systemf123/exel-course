97377d1d9b7cdade84a1a2e091fe25c778ff0ac5
Artem <my@bk.ru>
2020-09-30 13:47:40 +0300

    Finish bse table logic

--- src/core/ExelComponent.js
+++ src/core/ExelComponent.js
@@ -4,10 +4,25 @@ export class ExelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubsribes = []
+    this.prepare()
   }
+
+  prepare() {}
+
   // Возвразает шаблон компонента
   toHTML() {
     return ''
+  }
+
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubsribes.push(unsub)
   }
 
   init(event) {
@@ -16,5 +31,6 @@ export class ExelComponent extends DomListener {
 
   destroy() {
     this.removeDOMListeners()
+    this.unsubsribes.forEach(unsub => unsub())
   }
 }
